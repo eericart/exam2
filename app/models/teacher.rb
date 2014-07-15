204b2acb3922aa9be634cc2879fcad410bf676ef
Ernesto Ricart <ernest2193@gmail.com>
2014-07-15 10:53:41 -0400

    update Teacher model

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,2 +1,8 @@
 class Teacher < ActiveRecord::Base
+  has_many :subjects
+   accepts_nested_attributes_for :subjects,:allow_destroy => true,
+
+  def amount_subjects
+    return 0 if subjects.empty?
+  end
 end
